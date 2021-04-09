f8eb9990843f894b30b72b4f649f0f068bbfc877
Brenden <52762708+brendenmoore@users.noreply.github.com>
2021-04-09 17:10:45 -0400

    add actions to podcast

--- schemas/podcast.js
+++ schemas/podcast.js
@@ -1,23 +1,24 @@
 export default {
-    name: 'podcast',
-    title: 'Podcast',
-    type: 'document',
-    fields: [
-        {
-            name: 'title',
-            title: 'Title',
-            type: 'string',
-        },
-        {
-            name: 'description',
-            title: 'Description',
-            type: 'text',
-        },
-        {
-            name: 'links',
-            title: 'Subscription Links',
-            type: 'array',
-            of: [{ type: 'link' }],
-        },
-    ],
+  name: "podcast",
+  title: "Podcast",
+  type: "document",
+  __experimental_actions: ["update", "publish"],
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "text",
+    },
+    {
+      name: "links",
+      title: "Subscription Links",
+      type: "array",
+      of: [{ type: "link" }],
+    },
+  ],
 }
